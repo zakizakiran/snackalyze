c4fe90496e19aca679dbf32493ca8343d3a0eb13
zakizakiran <zzakiran4@gmail.com>
2024-12-04 08:26:38 +0700

    fixed about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,6 +1,6 @@
 const AboutPage = () => {
   return (
-    <div className="flex flex-col items-center lg:flex-row-reverse px-4">
+    <div className="flex flex-col items-center lg:flex-row-reverse px-4 py-12">
       <div className="m-auto">
         <img
           src="/images/about_illustration.svg"
@@ -11,10 +11,10 @@ const AboutPage = () => {
       <div className="lg:w-1/2 lg:pr-32">
         <div className="mb-8">
           <p className="text-sm mb-2 font-bold text-primary">About Us</p>
-          <h1 className="text-xl lg:text-3xl font-poppinsMedium mb-4">
+          <h1 className="text-2xl lg:text-3xl font-poppinsMedium mb-4">
             To innovate in order to improve Indonesia's nutritional needs.
           </h1>
-          <p>
+          <p className="text-gray-500">
             At Snackalyze, we believe that innovation holds the key to
             addressing one of Indonesia's most pressing challenges: improving
             nutritional awareness and choices. With a diverse culinary culture
@@ -23,10 +23,10 @@ const AboutPage = () => {
           </p>
         </div>
         <div></div>
-        <h1 className="text-xl lg:text-3xl font-poppinsMedium mb-4">
+        <h1 className="text-2xl lg:text-3xl font-poppinsMedium mb-4">
           Our Mission
         </h1>
-        <p>
+        <p className="text-gray-500">
           Our mission is to empower individuals with the tools they need to make
           informed dietary decisions. By leveraging cutting-edge image
           recognition technology, Snackalyze simplifies the process of
