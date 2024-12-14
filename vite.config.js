9d6f4528f52820ce7fb48a3d4d726d19206888e5
zakizakiran <zzakiran4@gmail.com>
2024-12-14 07:06:11 +0700

    fixed warning log

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "source-map-js": "source-map",
+      path: "path-browserify",
+    },
+  },
   server: {
     proxy: {
       "/api": {
@@ -12,5 +19,5 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
 });
